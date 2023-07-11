83995a453f0117b1078ce2f8464f8fd47478b039
Shakil Ahmed <shakilahmed0888@gmail.com>
2023-07-11 23:51:27 +0530

    bug fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 const connectDatbase = require("./db");
 const dotenv = require("dotenv");
 const router = require("./router/CreateUser.js");
